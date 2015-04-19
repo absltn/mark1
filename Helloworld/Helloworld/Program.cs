66400fae9aab7fa33b6ac16a4841f2dc88fe32d7
absltn <miet.kremerov@gmail.com>
2015-04-19 10:29:31 +0300

    Hello world
    test commit

---
+++ Helloworld/Helloworld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+            int testInt = 10;
+            Console.WriteLine("Hello world! ");
+            Console.WriteLine(testInt);
+        }
+    }
+}
