6e8baae78e64bd8f8c31d3c15ead1249aaf93934
absltn <miet.kremerov@gmail.com>
2015-04-20 21:19:42 +0300

    Lab2 finish
    User defined values tested

--- DNET lab2/DNET lab2/Program.cs
+++ DNET lab2/DNET lab2/Program.cs
@@ -10,9 +10,16 @@ namespace DNET_lab2
     {
         enum Color
         {
-            Red = 4,
+            Red = 5,
             Green = 3,
             Blue = 7
+        }
+        struct tStruct
+        {
+            public string name;
+            public int age;
+            public char sex;
+
         }
         static void Main(string[] args)
         {
@@ -22,8 +29,16 @@ namespace DNET_lab2
             char myC = 'a';
             Console.WriteLine("{0}, {1}, {2}",myDouble,myInt,myC);
             
+            // user defined value types
+
             Color myColor = Color.Red;
             Console.WriteLine("{0} and its integer value = {1}",myColor, (int)myColor);
+
+            tStruct myStruct;
+            myStruct.age = 28;
+            myStruct.sex = 'M';
+            myStruct.name = "Alex";
+            Console.WriteLine("{0}, {1}, {2}", myStruct.name, myStruct.sex, myStruct.age);
         }
     }
 }
