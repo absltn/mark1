83610e3d0a01d103a2f5e8b34452efac6177e893
absltn <miet.kremerov@gmail.com>
2015-04-20 21:11:03 +0300

    Lab2

---
+++ DNET lab2/DNET lab2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNET_lab2
+{
+    class Program
+    {
+        enum Color
+        {
+            Red = 4,
+            Green = 3,
+            Blue = 7
+        }
+        static void Main(string[] args)
+        {
+            int myInt = 4;
+            double myDouble = myInt + 0.3;
+            myInt = (int)myDouble;
+            char myC = 'a';
+            Console.WriteLine("{0}, {1}, {2}",myDouble,myInt,myC);
+            
+            Color myColor = Color.Red;
+            Console.WriteLine("{0} and its integer value = {1}",myColor, (int)myColor);
+        }
+    }
+}
