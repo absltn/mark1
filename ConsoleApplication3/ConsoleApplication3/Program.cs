042093a960430eb94f705f1a7eef2bacc04aa56b
absltn <miet.kremerov@gmail.com>
2015-07-12 23:56:00 +0300

    several labs finished

---
+++ ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 2, b = 4.5;
+            double x;
+            string usr = Console.ReadLine();
+            if (double.TryParse(usr, out x))
+            {
+                if ((x >= a) && (x <= b))
+                {
+                    Console.WriteLine("X in AB");
+                }
+                else
+                {
+                    Console.WriteLine("X not in AB");
+                }
+            }
+            else Console.WriteLine("Parse problem");
+            do
+                Console.WriteLine(x);
+            while (++x <= 10);
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+                Console.WriteLine(i);
+            }
+            ArrayList number = new ArrayList();
+
+
+        }
+    }
+}
